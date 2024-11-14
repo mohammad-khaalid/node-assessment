fb2d4af3a1a42c51e47b43cc080d2311d6df59b7
mohammad-khaalid <mohammad.khalid@stackroof.com>
2024-11-14 17:39:13 +0530

    Test: support newline as delimiter

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -19,4 +19,10 @@ test('returns the sum of multiple comma-separated numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
   });
 
+ test('handles newlines as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+  
+
+  
   
