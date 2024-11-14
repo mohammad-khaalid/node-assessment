61be1a18048f1bedace677ef360d5a9093f5e4ad
mohammad-khaalid <mohammad.khalid@stackroof.com>
2024-11-14 17:45:45 +0530

    Test: throw exception for negative numbers

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -27,7 +27,9 @@ test('supports different delimiters', () => {
     expect(add("//;\n1;2")).toBe(3); 
     expect(add("//|\n1|2|3")).toBe(6);
   });
-
+test('throws an exception for negative numbers', () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed: -2, -4");
+  });
   
   
   
