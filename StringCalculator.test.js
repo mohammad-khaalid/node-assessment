93c69ce18accbf026df2b9887b594d6f8fb604c4
mohammad-khaalid <mohammad.khalid@stackroof.com>
2024-11-14 17:40:26 +0530

    Test: support custom delimiters

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -19,10 +19,15 @@ test('returns the sum of multiple comma-separated numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
   });
 
- test('handles newlines as delimiters', () => {
+test('handles newlines as delimiters', () => {
     expect(add("1\n2,3")).toBe(6);
   });
   
+test('supports different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3); 
+    expect(add("//|\n1|2|3")).toBe(6);
+  });
 
+  
   
   
